0535241cb225a95889b3afbaaf88ac63867fefe5
Ian Baker <admin@ianbaker.me>
2020-01-13 22:14:47 +0000

    Added proxy and template examples

--- MockApi/Program.cs
+++ MockApi/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using WireMock.Matchers.Request;
-using WireMock.Net.StandAlone;
+using WireMock.Matchers;
 using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
 using WireMock.Server;
@@ -12,16 +11,32 @@ namespace MockApi
     {
         static void Main(string[] args)
         {
+            var settings = new FluentMockServerSettings
+            {
+                Urls = new[] { "https://localhost:9095/" },
+                StartAdminInterface = true,
+                ProxyAndRecordSettings = new ProxyAndRecordSettings
+                {
+                    Url = "https://samples.openweathermap.org",
+                    SaveMapping = true,
+                    SaveMappingToFile = true,
+                    SaveMappingForStatusCodePattern = "2xx"
+                }
+            };
+
+            var proxyServer = FluentMockServer.Start(settings);
+
             var server = FluentMockServer.Start(new FluentMockServerSettings()
             {
-                Urls = new[] { "http://localhost:9095" },
+                Urls = new[] { "http://localhost:9096/" },
                 StartAdminInterface = true
             });
 
-            server.Given(Request.Create().WithPath("/some/thing").UsingGet())
+            server.Given(Request.Create().WithPath("/some/thing").WithParam("name", new ExactMatcher("abc")))
                 .RespondWith(Response.Create().WithStatusCode(201)
                     .WithHeader("Content-Type", "text/plain")
-                    .WithBody("Hello world!"));
+                    .WithBody($"Hello {{{{request.query.name}}}} it's {DateTime.Now}!")
+                    .WithTransformer());
 
             Console.WriteLine("Press any key to stop the server");
             Console.ReadKey();
